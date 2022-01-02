c73c1326ed2c5d931b35d3beb784391041ecc383
developer <developer@industria-i.com.br>
2022-01-02 13:39:42 +0000

    [FEATURE] Create the console app and print the result on console.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+
+namespace FizzBuzz;
+
+// See https://aka.ms/new-console-template for more information
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var fizzBuzz = new FizzBuzz();
+        Console.WriteLine(fizzBuzz.GetFizzBuzz().Aggregate((a, b) => a + "\n" + b));
+    }
+}
+// </SnippetProgram>
