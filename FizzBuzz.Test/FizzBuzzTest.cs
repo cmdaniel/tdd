dacfeb3ad57ec8fc3decc6bd072c8c397a92563e
developer <developer@industria-i.com.br>
2022-01-02 13:16:10 +0000

    [DEPLOY] Start Program.
    Create a program to generate 100 items with sequencial numbers.
    However, every multiple of 3 must has the Fizz value, and every multiple of 5 must be Buzz.
    Finally, freate Unit tests for the program.

---
+++ FizzBuzz.Test/FizzBuzzTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace FizzBuzz.Test;
+
+public class FizzBuzzTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void ShouldHave100Items()
+    {
+        var fizzBuzz = new FizzBuzz();
+        var result = fizzBuzz.GetFizzBuzz();
+        Assert.Equals(100, result.Count);
+    }
+}
---
+++ FizzBuzz/readme.md
@@ -0,0 +1,4 @@
+# Create a program to generate 100 items with sequencial numbers
+# However, every multiple of 3 must has the Fizz value
+# and every multiple of 5 must be Buzz
+# Create Unit tests for the program.
