8cdc5f1717653dc2361765874c38f1e0fdc5dd4e
developer <developer@industria-i.com.br>
2022-01-02 14:45:07 +0000

    [REFACTOR] New simplified main console program format. sDg!

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,14 +1,7 @@
 ﻿using System.Linq;
-
-namespace FizzBuzz;
+using FizzBuzz;
 
 // See https://aka.ms/new-console-template for more information
-public static class Program
-{
-    public static void Main(string[] args)
-    {
-        var fizzBuzz = new FizzBuzz();
-        Console.WriteLine(fizzBuzz.GetFizzBuzz().Aggregate((a, b) => a + "\n" + b));
-    }
-}
-// </SnippetProgram>
+
+var fizzBuzz = new FizzBuzzSeries();
+Console.WriteLine(fizzBuzz.GetFizzBuzz().Aggregate((a, b) => a + "\n" + b));
